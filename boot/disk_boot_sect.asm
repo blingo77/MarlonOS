784ad1488722c289db65b15661912c130e0b2743
Brandon Lingo <brandonlingo0@gmail.com>
2024-07-12 02:15:07 -0700

    complete disk loader

---
+++ boot/disk_boot_sect.asm
@@ -0,0 +1,44 @@
+
+load_disk:
+
+    push dx             ; push dx into stack, dh contains the amt of sectors to read, dl has the drive number
+
+    mov ah, 0x02           ; int 0x13 BIOS 'read' function requires that ah register have 0x02
+    mov al, dh             ; move amt of sectors to read into al
+
+    ; CHS (Cylinder, head, sector) even though we wont write it in that order
+    mov cl, 0x02           ; cl = the sector, is set to sector 2, sector 1 = boot loader, sector 2 = next available sector
+    mov ch, 0x00           ; ch = the cylinder, is set to 0
+    mov dh, 0x00           ; dh = the head, is set ti 0
+
+    int 0x13               ; BIOS Interupt
+    jc disk_error          ; if the carry flag is set to 1, then jump to disk_error
+
+    pop dx                 ; restore the DX register when we pushed it, giving us back dh = 2, which is the amt of sectors we want read
+    cmp al, dh             ; the 0x13 BIOS interrupt will auto store the amt of sectors read into AL register
+    jne sector_error       ; if both AL and DH arent ==, then jump to sector error
+
+    ret
+
+disk_error:
+
+    mov si, DISK_ERROR     ; si will be passed to print
+    call print
+
+    call print_newline
+
+    mov dh, ah             ; DH register will recieve AH, AH holds the error code
+    call print_hex         ; takes DX as parameter, DH & DL -> DX 
+    jmp disk_loop
+
+    ret
+
+sector_error:
+    mov si, SECTOR_ERROR
+    call print
+
+disk_loop:
+    jmp $
+
+DISK_ERROR: db "DISK READ ERROR",0
+SECTOR_ERROR: db "Wrong # of sectors read", 0
--- boot/main_boot_sect.asm
+++ boot/main_boot_sect.asm
@@ -4,23 +4,26 @@ BITS 16                             ;instructions will be generated in 16-bit in
 
 start:
 
-    mov si, message                 ; sets the message mem adress into si register
-    call print                      ; calls sub routine print which is in file ./print_boot_sect.asm
+    mov bp, 0x8000                  ; set the stack at 0x8000 which moves it safely away from us
+    mov sp, bp                      ; sets stack pointer to base pointer, making stack start at 0x8000
 
-    call print_newline              ; prints new line, in same file as print
+    mov bx, 0x9000                  ;sets destination buffer to read at segment 0x0000, offset 0x9000 es:bx = 0x0000:0x9000 = 0x9000
+    mov dh, 2                       ; number of sectors to read
+    call load_disk
 
-    mov si, GOODBYE
-    call print
+    mov dx, [0x9000]                ; call first loaded sector, 0xdada
+    call print_hex                  ; dx is passed into print_hex
 
     call print_newline
 
-    mov dx, 0x12fe
+    mov dx, [0x9000 + 512]          ; add 512 bytes giving us the second loaded sector, 0xface
     call print_hex
 
     jmp $
     
 %include "./print_boot_sect.asm"    ; includes the file that has print function
 %include "./print_hex_boot.asm"
+%include "./disk_boot_sect.asm"
 
 message: 
     db 'hello World', 0
@@ -29,3 +32,7 @@ GOODBYE:
 
 times 510-($ - $$) db 0             ; fills the rest of the 512 byte sector with zeroes
 dw 0xAA55                           ; will be flipped as 0x55AA and is placed at the end of the 512 byte sector, dw is two bites
+
+;boot sector = sector 1
+times 256 dw 0xdada ; sector 2 = 512 bytes, when booting this sector it will print 0xdada
+times 256 dw 0xface ; sector 3 = 512 bytes, when booting this sector it will print 0xface
