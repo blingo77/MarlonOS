4ffb4ecc31b1eb5c2b0b967b83858ecc91c2cc92
Brandon Lingo <brandonlingo0@gmail.com>
2024-07-23 02:04:25 -0700

    add print_char to screen.c

--- drivers/screen.c
+++ drivers/screen.c
@@ -5,6 +5,80 @@
 int get_offset(int col, int row);
 int get_offset_row(int offset);
 int get_offset_col(int offset);
+void set_cursor_offset(int offset);
+int get_cursor_offset();
+int print_char(char c, int col, int row, char attribute);
+
+int print_char(char c, int col, int row, char attribute)
+{
+    unsigned char *video_mem = (unsigned char*) VID_MEM_ADDRESS;
+    if(!attribute) attribute = WHITE_ON_BLACK;
+
+    // FOR ERROR CHECKING
+    if (col >= MAX_COLS || row >= MAX_ROWS)
+    {
+        video_mem[2 * (MAX_COLS) * (MAX_ROWS) - 2] = 'E';           // sets the character byte to an E
+        video_mem[2 * (MAX_COLS) * (MAX_ROWS) - 1] = RED_ON_WHITE;  // sets the attribute byte to red font and white foreground
+    }
+
+    int offset;
+    if(col >= 0 && row >=0)
+    { 
+        offset = get_offset(col, row);
+    }
+    else { offset = get_cursor_offset(); }
+
+    if(c == '\n')                           // if the character is a new line 
+    {
+        row = get_offset_row(offset);       // gets the row currently at
+        offset = get_offset(0, row + 1);    // adds 1 to go the next row and goes to column 0, setting offset to the new
+    }
+    else
+    {
+        video_mem[offset] = c;              // Sets the character byte to the character
+        video_mem[offset + 1] = attribute;  // Sets the attribute byte to the attribute declared ealier
+        offset += 2;                        // adds two, to move to next character cell position
+    }
+
+    set_cursor_offset(offset);              // updates the cursor position after a character is printed
+
+    return offset;
+}
+
+int get_cursor_offset()
+{
+    port_byte_out(SCREEN_CONTROL_REGISTER, 14);                     // Requesting the high byte
+    int cursor_offset = port_byte_in(SCREEN_DATA_REGISTER) << 8;    // retrieve the high byte and shift it left in the offset
+
+    port_byte_out(SCREEN_CONTROL_REGISTER, 15);                     // request the low byte
+    cursor_offset += port_byte_in(SCREEN_DATA_REGISTER);            // add the low byte into the cursor offset
+
+    return cursor_offset * 2;   // multiplies the position by the size of the character cell
+}
+
+void clear_screen()
+{
+    int screen_size = MAX_COLS * MAX_ROWS;
+    char *screen = VID_MEM_ADDRESS;
+
+    for(int i = 0; i < screen_size; i++)
+    {
+        screen[i * 2] = ' ';                // Sets the character byte to a blank 
+        screen[i * 2 + 1] = WHITE_ON_BLACK; // Sets the attribute byte to white font and black foreground
+    }
+}
+
+void set_cursor_offset(int offset)
+{
+    /* positions the text cursor on a VGA display*/
+
+    offset /= 2;        // get the character cell position instead of byte offset
+
+    port_byte_out(SCREEN_CONTROL_REGISTER, 14);                          // requesting to modify the high byte 
+    port_byte_out(SCREEN_DATA_REGISTER, (unsigned char)(offset >> 8));   // Modifys the highbyte
+    port_byte_out(SCREEN_CONTROL_REGISTER, 15);                          // requests the low byte
+    port_byte_out(SCREEN_DATA_REGISTER, (unsigned char)(offset & 0xff)); // modifys the low byte
+}
 
 int get_offset(int col, int row)
 {
